26be4a05a77f09fc1da5830fe243606d85993e04
Nourhan ALAshkar <norhanelashker399@gmail.com>
2026-06-11 05:03:59 +0300

    First Playwright project

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,22 @@
 import { test, expect } from '@playwright/test';
 
-test('Open Playwright Website', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('User can successfully log in', async ({ page }) => {
+  // 1. Navigate to the login page
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-  await expect(page).toHaveTitle(/Playwright/);
+  // 2. Fill out the credentials
+  //await page.getByLabel('Username or email').fill('admin');
+  //await page.getByLabel('Password').fill('admin123');
+  await page.locator('input[name="username"]').fill('Admin');
+  await page.locator('input[name="password"]').fill('admin123');
+
+  // 3. Click the login button
+  await page.getByRole('button', { name: 'Login' }).click();
+  //await page.getByRole('button', { name: 'Sign In' }).click();
+
+  // 4. Assert login success (e.g., URL redirection or visibility of an element)
+  //await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
+  await expect(page).toHaveURL(/dashboard/);
+  //await expect(page.getByText('Welcome back!')).toBeVisible();
 });
+
